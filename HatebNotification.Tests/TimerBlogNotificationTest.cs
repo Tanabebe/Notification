2bc9990a92f6c8cc056d8aa0b589002e0dd49956
Tanabebe <nob@tanabebe.tech>
2021-08-05 23:35:38 +0900

    xUnit対応
    試しに1メソッドテスト

---
+++ HatebNotification.Tests/TimerBlogNotificationTest.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using tanabebe.tech.function;
+using Xunit;
+
+namespace HatebNotification.Tests
+{
+    public class CompareToBlogDayTestDataClass : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { new DateTime(2008, 6, 1, 7, 47, 0) };
+            yield return new object[] { DateTime.Now.AddYears(-1) };
+            yield return new object[] { DateTime.Now.AddMonths(-1) };
+            yield return new object[] { DateTime.Now };
+            yield return new object[] { DateTime.Now.AddHours(1) };
+            yield return new object[] { DateTime.Now.AddDays(2) };
+        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+    public class TimerBlogNotificationTest
+    {
+        [Theory]
+        [ClassData(typeof(CompareToBlogDayTestDataClass))]
+        public void CompareToBlogDayTest(DateTime date)
+        {
+            var published = TimerBlogNotification.CompareToBlogDay(date);
+            Assert.InRange(published, -1, 1);
+        }
+    }
+}
